eb7adda047c853a2143be8bfaf00b4c9259999b2
Ivan Montiel <idmontie@gmail.com>
2018-09-26 19:45:27 -0700

    Fix auth import

--- packages/jwt-auth/src/jwt-auth-context.js
+++ packages/jwt-auth/src/jwt-auth-context.js
@@ -1,8 +1,10 @@
+const Auth = require('@clarityhub/harmony-server');
+
 module.exports = (bottle) => {
     return async ({ req }) => {
         const { authorization } = req.headers;
 
-        const auth = new Auth({ id });
+        const auth = new Auth({ id: authorization });
 
         if (bottle) {
             bottle.service('Auth', () => auth);
