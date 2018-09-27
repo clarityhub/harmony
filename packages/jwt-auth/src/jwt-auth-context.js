35be45147e04cdf6034c7e5c5e7a14cbd41ce17f
Ivan Montiel <idmontie@gmail.com>
2018-09-26 21:42:29 -0700

    Add middleware to controller

--- packages/jwt-auth/src/jwt-auth-context.js
+++ packages/jwt-auth/src/jwt-auth-context.js
@@ -4,6 +4,7 @@ module.exports = (cb) => {
     return async ({ req }) => {
         const { authorization } = req.headers;
 
+        // Todo, need Different Auth types, like NotAuthed for when no jwt is provided
         const auth = new Auth({ id: authorization });
 
         return cb(auth);
--- packages/server/src/Controller.js
+++ packages/server/src/Controller.js
@@ -3,6 +3,12 @@ module.exports = class Controller {
         this.ioc = ioc;
     }
 
+    async middleware(Middleware) {
+        const middleware = new Middleware(this.ioc);
+
+        await middleware.check();
+    }
+
     validate(obj, ClassValidator) {
         const v = new ClassValidator();
         return v.check(obj);
